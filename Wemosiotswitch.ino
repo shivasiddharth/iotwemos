d33230eb0607929212bbdd4c83e16efe9e5a4f9d
shivasiddharth <ushivasiddharth@gmail.com>
2017-01-01 20:56:04 +0000

    Update Wemosiotswitch.ino

--- Wemosiotswitch.ino
+++ Wemosiotswitch.ino
@@ -4,8 +4,8 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 
-const char* ssid = "Siddhy";             //Set your wifi network name(ssid)
-const char* password = "Siddhy1990";                //Set your router password
+const char* ssid = "******";             //Set your wifi network name(ssid)
+const char* password = "*******";                //Set your router password
  
 int RelayPin = D1;          //Relay Shield is controlled by pin 1 (D1)
 WiFiServer server(80);
