c7517546863b012ecf9293ffe1e9ba147dd3df76
shivasiddharth <ushivasiddharth@gmail.com>
2018-10-06 02:10:57 +0100

    Add files via upload

---
+++ Google_Home_RF_Control.ino
@@ -0,0 +1,173 @@
+/***************************************************
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board & Feather
+  ----> https://www.adafruit.com/product/2471
+  ----> https://www.adafruit.com/products/2821
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+/********************************************************************
+   Modified by Sid for Sid's E Classroom
+   https://www.youtube.com/c/SidsEClassroom
+ ********************************************************************/
+
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <RCSwitch.h>
+
+
+//Change the pins according to the choice of your board
+//Get the pin maps from this link https://escapequotes.net/esp8266-wemos-d1-mini-pins-and-diagram/ for Wemos
+#define RCPIN 0 //Corresponds to D3 on Wemos D1
+
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "********"  //Change accordingly
+#define WLAN_PASS       "********" //Change accordingly
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+//Add your Adafruit.IO username and key. It can be found under settings tab
+#define AIO_USERNAME    "************"
+#define AIO_KEY         "*********************"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+// Setup a feed called 'AlexaAndHome' for subscribing to changes.
+Adafruit_MQTT_Subscribe AlexaAndHome = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/AlexaAndHome");
+
+/****************************** RC Switch ***************************************/
+RCSwitch mySwitch = RCSwitch();
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  
+  mySwitch.enableTransmit(RCPIN);
+  // Optional set pulse length.
+  // mySwitch.setPulseLength(320);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+  // mySwitch.setProtocol(2);
+  
+  // Optional set number of transmission repetitions.
+  // mySwitch.setRepeatTransmit(15);
+
+  
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&AlexaAndHome);
+
+}
+
+uint32_t x = 0;
+
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets' busy subloop
+  // try to spend your time here
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+
+
+    if (subscription == &AlexaAndHome) {
+      Serial.print(F("Got from AlexaAndHome Feed: "));
+
+      Serial.println((char *)AlexaAndHome.lastread);
+
+      if (strcmp((char *)AlexaAndHome.lastread, "on monitor") == 0) {
+        mySwitch.send(13911311, 24);
+        }
+      if (strcmp((char *)AlexaAndHome.lastread, "off monitor") == 0) {
+        mySwitch.send(13911310, 24);
+        }  
+      
+    }
+
+  }
+}
+// ping the server to keep the mqtt connection alive
+// NOT required if you are publishing once every KEEPALIVE seconds
+/*
+  if(! mqtt.ping()) {
+  mqtt.disconnect();
+  }
+*/
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
